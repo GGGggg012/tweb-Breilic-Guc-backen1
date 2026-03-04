01b80204cf8e4093b1190f4ef0cd3498144e8710
MaxiMGuc <maksym.huk@gmail.com>
2026-03-04 09:30:00 +0300

    Add OrderStatus enum: Pending, Paid, Cancelled, Delivered, Refunded

---
+++ eAviaSales.Domain/Entities/OrderStatus.cs
@@ -0,0 +1,11 @@
+namespace eAviaSales.Domain.Entities
+{
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Paid = 1,
+        Cancelled = 2,
+        Delivered = 3,
+        Refunded = 4
+    }
+}

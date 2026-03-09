f5f454f3da91eb53594332bbee748ba48ae516ec
GGGggg012 <ggpuregg12@gmail.com>
2026-03-09 11:30:00 +0300

    Add user authentication DTOs: register, login, profile

---
+++ eAviaSales.Domain/Models/UserLoginDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace eAviaSales.Domain.Models
+{
+    public class UserLoginDto
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
